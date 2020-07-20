294887ea55d6df45792981e2277ecf7f7b119670
Jason Webb <46631010+JasonMWebb@users.noreply.github.com>
2020-07-20 19:56:17 -0400

    Stubbed unit tests and sample projects.

---
+++ RCommon.Samples.MultiThreadedConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RCommon.Samples.MultiThreadedConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
