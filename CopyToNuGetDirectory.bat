1098996bf9cea0207e4a928a48a4992f372feee6
Jason Webb <46631010+JasonMWebb@users.noreply.github.com>
2020-07-25 00:03:33 -0400

    Updated NuGet packages.

---
+++ CopyToNuGetDirectory.bat
@@ -0,0 +1,9 @@
+xcopy RCommon\bin\Debug\*.nupkg C:\Users\jason\Documents\Code\Reactor2\RCommon\NuGetPackages\ /y
+xcopy RCommon.DependencyInjection\bin\Debug\*.nupkg C:\Users\jason\Documents\Code\Reactor2\RCommon\NuGetPackages\ /y
+xcopy RCommon.DependencyInjection.CastleWindsor\bin\Debug\*.nupkg C:\Users\jason\Documents\Code\Reactor2\RCommon\NuGetPackages\ /y
+xcopy RCommon.DependencyInjection.Microsoft\bin\Debug\*.nupkg C:\Users\jason\Documents\Code\Reactor2\RCommon\NuGetPackages\ /y
+xcopy RCommon.DependencyInjection.Autofac\bin\Debug\*.nupkg C:\Users\jason\Documents\Code\Reactor2\RCommon\NuGetPackages\ /y
+xcopy RCommon.ObjectAccess.EfCore\bin\Debug\*.nupkg C:\Users\jason\Documents\Code\Reactor2\RCommon\NuGetPackages\ /y
+xcopy RCommon.ObjectAccess.NHibernate\bin\Debug\*.nupkg C:\Users\jason\Documents\Code\Reactor2\RCommon\NuGetPackages\ /y
+xcopy C:\Users\jason\Documents\Code\Reactor2\RCommon\NuGetPackages\*.nupkg "C:\Users\jason\OneDrive - NTeris\Code\Nuget Packages\RCommon\" /y
+pause
