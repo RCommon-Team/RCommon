d381a3fa0c986a930aa78c2e86e2e2ef903bf7eb
Jason Webb <46631010+JasonMWebb@users.noreply.github.com>
2021-03-25 21:16:42 -0400

    Updated samples solution to .net 5

---
+++ RCommon.ObjectAccess.Dapper/DapperConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using RCommon.Configuration;
+using RCommon.DependencyInjection;
+using RCommon.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCommon.ObjectAccess.Dapper
+{
+    public class DapperConfiguration : IObjectAccessConfiguration
+    {
+        private List<string> _dbContextTypes = new List<string>();
+
+        /// <summary>
+        /// Called by RCommon <see cref="Configure"/> to configure data providers.
+        /// </summary>
+        /// <param name="containerAdapter">The <see cref="IContainerAdapter"/> instance that allows
+        /// registering components.</param>
+        public void Configure(IContainerAdapter containerAdapter)
+        {
+
+            // EF Core Repository
+            containerAdapter.AddGeneric(typeof(IAsyncCrudRepository<>), typeof(DapperRepository<>));
+
+            // Registered DbContexts
+            foreach (var dbContext in _dbContextTypes)
+            {
+                containerAdapter.AddTransient(Type.GetType(dbContext), Type.GetType(dbContext));
+            }
+
+
+        }
+
+
+        public IObjectAccessConfiguration UsingDbContext<TDbContext>()
+            where TDbContext : RCommonDbContext
+        {
+            var dbContext = typeof(TDbContext).AssemblyQualifiedName;
+            _dbContextTypes.Add(dbContext);
+
+            //_dbContextType = Activator.CreateInstance(typeof(TDbContext));
+
+            return this;
+        }
+    }
+}
