efc23c4b8f4cd7ceabc090287fb7b6606405df13
Jason Webb <46631010+JasonMWebb@users.noreply.github.com>
2023-06-16 14:47:51 -0600

    Adding ignored file.

---
+++ Src/RCommon.Persistence.EfCore/IEFCoreConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RCommon.Persistence.EFCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCommon
+{
+    public interface IEFCoreConfiguration : IObjectAccessConfiguration
+    {
+        IEFCoreConfiguration AddDbContext<TDbContext>(string dataStoreName, Action<DbContextOptionsBuilder>? options) where TDbContext : RCommonDbContext;
+    }
+}
