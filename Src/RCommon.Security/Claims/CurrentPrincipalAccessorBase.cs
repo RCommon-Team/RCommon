13ef5997e2705cf63673926a02c19108788ed5cf
Jason Webb <46631010+JasonMWebb@users.noreply.github.com>
2023-11-10 10:54:28 -0700

    Updated meta data in projects to reflect version changes.

--- Src/RCommon.Security/Claims/CurrentPrincipalAccessorBase.cs
+++ Src/RCommon.Security/Claims/CurrentPrincipalAccessorBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RCommon.Security.Claims
