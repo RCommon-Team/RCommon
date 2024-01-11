78c00508d4c8095e263e6d4b2a60f1beabac8913
Jason Webb <46631010+JasonMWebb@users.noreply.github.com>
2024-01-10 20:43:36 -0700

    Added .NET6, 7, 8 Unit tests.

---
+++ Tests/RCommon.DotNet6.Tests/DotNetTests.cs
@@ -0,0 +1,21 @@
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace RCommon.DotNet6.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Can_Use_Dapper()
+        {
+            var services = new ServiceCollection();
+            var target = new DapperConfiguration(services);
+            Assert.That(target, Is.Not.Null);
+        }
+    }
+}
