c11a61f618df8890526f1cf62617572432fa1fb4
Jason Webb <46631010+JasonMWebb@users.noreply.github.com>
2024-01-20 16:23:42 -0700

    Added wolverine wrappers and example.

---
+++ Src/RCommon.Wolverine/Subscribers/IWolverineEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using Wolverine;
+using RCommon.EventHandling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCommon.MassTransit.Subscribers
+{
+    public interface IWolverineEventHandler
+    {
+    }
+
+    public interface IWolverineEventHandler<in TDistributedEvent> : IWolverineEventHandler
+        where TDistributedEvent : class, ISerializableEvent
+    {
+        Task HandleAsync(TDistributedEvent distributedEvent, CancellationToken cancellationToken = default);
+    }
+}
---
+++ Src/RCommon.Wolverine/Subscribers/WolverineEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using RCommon.EventHandling;
+using RCommon.EventHandling.Subscribers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wolverine;
+
+namespace RCommon.MassTransit.Subscribers
+{
+    public class WolverineEventHandler<TEvent> : IWolverineEventHandler<TEvent>, IWolverineHandler
+        where TEvent : class, ISerializableEvent
+    {
+        private readonly ISubscriber<TEvent> _subscriber;
+
+        public WolverineEventHandler(ISubscriber<TEvent> subscriber)
+        {
+            _subscriber = subscriber;
+        }
+
+        public async Task HandleAsync(TEvent distributedEvent, CancellationToken cancellationToken = default)
+        {
+            Console.WriteLine("{0} handling event {1} from MassTransit", new object[] { this.GetGenericTypeName(), distributedEvent });
+            await _subscriber.HandleAsync(distributedEvent);
+        }
+    }
+}
