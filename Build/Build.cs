3ef2608c65442b7b48af9ea576b90975e30b4306
Jason Webb <jason.webb@leadventure.com>
2024-04-23 13:34:34 -0600

    Version bump.

--- Build/Build.cs
+++ Build/Build.cs
@@ -135,7 +135,7 @@ class Build : NukeBuild
         {
             Log.Information("Generating NuGet packages for projects in solution");
             int commitNum = 0;
-            string NuGetVersionCustom = "2.0.0.873";
+            string NuGetVersionCustom = "2.0.0.874";
 
 
             //if it's not a tagged release - append the commit number to the package version
