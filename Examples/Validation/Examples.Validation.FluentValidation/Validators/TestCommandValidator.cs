026feeecd8ccd48579c7b91286a08788e06440bc
Jason Webb <jason.webb@leadventure.com>
2024-05-07 20:56:29 -0600

    Removing codebloat unit testing projects.

---
+++ Examples/Validation/Examples.Validation.FluentValidation/Validators/TestCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.ApplicationServices.CQRS.Validators
+{
+    public class TestCommandValidator : AbstractValidator<TestCommand>
+    {
+        public TestCommandValidator()
+        {
+            RuleFor(command => command.Message).NotNull().NotEmpty();
+        }
+    }
+}
