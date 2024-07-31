886b4a34e03d6cc4736e896849e914b91fcf81b0
Jason Webb <jason.webb@leadventure.com>
2024-07-31 00:25:03 -0600

    WIP

---
+++ Src/RCommon.Persistence.Caching/IPersistenceCachingBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCommon.Persistence.Caching
+{
+    public static class IPersistenceCachingBuilderExtensions
+    {
+        public static IPersistenceCachingBuilder Configure(this IPersistenceCachingBuilder builder)
+        {
+            
+            return builder;
+        }
+    }
+}
---
+++ Src/RCommon.Persistence.Caching/PersistenceCachingBuilder.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RCommon.Caching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCommon.Persistence.Caching
+{
+    public class PersistenceCachingBuilder : IPersistenceCachingBuilder
+    {
+        public PersistenceCachingBuilder(IRCommonBuilder builder)
+        {
+            Services = builder.Services;
+            RegisterServices(Services);
+        }
+
+        protected void RegisterServices(IServiceCollection services)
+        {
+
+        }
+
+        public IServiceCollection Services { get; }
+    }
+}
