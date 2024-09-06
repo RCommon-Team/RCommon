89d89dd71e692eef1398c71be6160c1996d9109a
Jason Webb <jason.webb@leadventure.com>
2024-09-05 21:09:46 -0600

    WIP: Added persistence caching projects.

---
+++ Src/RCommon.Persistence.Caching.MemoryCache/IPersistenceCachingBuilderExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using RCommon.Caching;
+using RCommon.MemoryCache;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCommon.Persistence.Caching.MemoryCache
+{
+    public static class IPersistenceCachingBuilderExtensions
+    {
+        public static IPersistenceCachingBuilder AddInMemoryPersistenceCaching(this IPersistenceCachingBuilder builder)
+        {
+            builder.Services.TryAddTransient<Func<PersistenceCachingStrategy, ICacheService>>(serviceProvider => strategy =>
+            {
+                switch (strategy)
+                {
+                    case PersistenceCachingStrategy.Default:
+                        return serviceProvider.GetService<InMemoryCacheService>();
+                    default:
+                        return serviceProvider.GetService<InMemoryCacheService>();
+                }
+            });
+            builder.Services.TryAddTransient<ICommonFactory<PersistenceCachingStrategy, ICacheService>, CommonFactory<PersistenceCachingStrategy, ICacheService>>();
+
+            builder.Services.Configure<CachingOptions>(x =>
+            {
+                x.CachingEnabled = true;
+                x.CacheDynamicallyCompiledExpressions = true;
+            });
+            return builder;
+        }
+
+        public static IPersistenceCachingBuilder AddDistributedMemoryPersistenceCaching(this IPersistenceCachingBuilder builder)
+        {
+            builder.Services.TryAddTransient<Func<PersistenceCachingStrategy, ICacheService>>(serviceProvider => strategy =>
+            {
+                switch (strategy)
+                {
+                    case PersistenceCachingStrategy.Default:
+                        return serviceProvider.GetService<DistributedMemoryCacheService>();
+                    default:
+                        return serviceProvider.GetService<DistributedMemoryCacheService>();
+                }
+            });
+            builder.Services.TryAddTransient<ICommonFactory<PersistenceCachingStrategy, ICacheService>, CommonFactory<PersistenceCachingStrategy, ICacheService>>();
+
+            builder.Services.Configure<CachingOptions>(x =>
+            {
+                x.CachingEnabled = true;
+                x.CacheDynamicallyCompiledExpressions = true;
+            });
+            return builder;
+        }
+    }
+}
--- Src/RCommon.Persistence.Caching.RedisCache/IPersistenceCachingBuilderExtensions.cs
+++ Src/RCommon.Persistence.Caching.RedisCache/IPersistenceCachingBuilderExtensions.cs
@@ -1,19 +1,30 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using RCommon.Caching;
+using RCommon.RedisCache;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RCommon.Persistence.Caching
+namespace RCommon.Persistence.Caching.RedisCache
 {
     public static class IPersistenceCachingBuilderExtensions
     {
-        public static IPersistenceCachingBuilder Configure(this IPersistenceCachingBuilder builder, Func<IServiceProvider, Func<PersistenceCachingStrategy, ICacheService>> cacheFactory)
+
+        public static IPersistenceCachingBuilder AddRedisPersistenceCaching(this IPersistenceCachingBuilder builder)
         {
-            builder.Services.TryAddTransient<Func<PersistenceCachingStrategy, ICacheService>>(cacheFactory);
+            builder.Services.TryAddTransient<Func<PersistenceCachingStrategy, ICacheService>>(serviceProvider => strategy =>
+            {
+                switch (strategy)
+                {
+                    case PersistenceCachingStrategy.Default:
+                        return serviceProvider.GetService<RedisCacheService>();
+                    default:
+                        return serviceProvider.GetService<RedisCacheService>();
+                }
+            });
             builder.Services.TryAddTransient<ICommonFactory<PersistenceCachingStrategy, ICacheService>, CommonFactory<PersistenceCachingStrategy, ICacheService>>();
 
             builder.Services.Configure<CachingOptions>(x =>
