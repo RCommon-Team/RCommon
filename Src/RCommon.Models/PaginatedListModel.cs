2e06554cc56876b6baca17f69ab92eaefb123322
Jason Webb <jason.webb@leadventure.com>
2024-11-21 23:17:02 -0700

    Removed unneeded dependency and simplified models. Removed need to serialize complex object and thus eliminated complexities surrounding serialization options.

--- Src/RCommon.Models/PaginatedListModel.cs
+++ Src/RCommon.Models/PaginatedListModel.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.Serialization;
 using System.Text;
-using RCommon.Collections;
 
 namespace RCommon.Models
 {
@@ -25,56 +24,30 @@ namespace RCommon.Models
             PaginateQueryable(source, paginatedListRequest, skipTotal);
         }
 
-        /// <summary>
-        /// Accepts <see cref="IPaginatedList{T}"/> and overrides total record count with the total count provided in the parameter. 
-        /// </summary>
-        /// <param name="source">Pre-filtered list of data.</param>
-        /// <param name="paginatedListRequest">Request model that contains populated state date for page number, page size, sort by, etc.</param>
-        /// <param name="totalCount">Total count of records contained by pre-filtered data source. This should be different than the actual record 
-        /// count in <see cref="IPaginatedList{T}"/></param>
-        /// <param name="skipSort">Instructions on whether or not to skip sorting. Default is false</param>
-        /// <remarks></remarks>
-        protected PaginatedListModel(IPaginatedList<TSource> source, PaginatedListRequest paginatedListRequest, int totalCount, bool skipSort = false)
-        {
-            PaginateList(source, paginatedListRequest, totalCount, skipSort);
-        }
-
         private IQueryable<TSource> Sort(IQueryable<TSource> source)
         {
 
-            if (this.SortExpression == null)
+            if (this._sortExpression == null)
             {
                 return source;
             }
 
             return SortDirection == SortDirectionEnum.Descending
-                ? source.OrderByDescending(this.SortExpression)
-                : source.OrderBy(this.SortExpression);
-        }
-
-        private IList<TSource> Sort(IPaginatedList<TSource> source)
-        {
-            
-            if (this.SortExpression == null)
-            {
-                return source;
-            }
-
-            var sortFunc = this.SortExpression.Compile(); // How much overhead is this?
-
-            var list = SortDirection == SortDirectionEnum.Descending
-                ? source.OrderByDescending(sortFunc).ToList()
-                : source.OrderBy(sortFunc).ToList();
-            return list;
+                ? source.OrderByDescending(this._sortExpression)
+                : source.OrderBy(this._sortExpression);
         }
 
         protected void PaginateQueryable(IQueryable<TSource> source, PaginatedListRequest paginatedListRequest, bool skipTotal = false, bool skipSort = false)
         {
+            if (source == null)
+            {
+                throw new ArgumentException("Source Data cannot be null");
+            }
+
             if (paginatedListRequest == null)
             {
-                return;
+                throw new ArgumentException("Request input cannot be null");
             }
-            Guard.IsNotNull(source, nameof(source));
 
             SortBy = paginatedListRequest.SortBy ?? "id";
             SortDirection = paginatedListRequest.SortDirection;
@@ -98,40 +71,12 @@ namespace RCommon.Models
             Items = CastItems(query).ToList();
         }
 
-        protected void PaginateList(IPaginatedList<TSource> source, PaginatedListRequest paginatedListRequest, int totalCount, 
-            bool skipSort = false)
-        {
-            if (paginatedListRequest == null)
-            {
-                return;
-            }
-            Guard.IsNotNull(source, nameof(source));
-
-            SortBy = paginatedListRequest.SortBy ?? "id";
-            SortDirection = paginatedListRequest.SortDirection;
-
-            PageSize = paginatedListRequest.PageSize;
-            PageNumber = paginatedListRequest.PageNumber;
-
-            if (totalCount > 0)
-            {
-                TotalCount = totalCount;
-                TotalPages = TotalCount / PageSize + (TotalCount % PageSize > 0 ? 1 : 0) ?? 1;
-            }
-
-            var query = skipSort ? source : Sort(source);
-
-            /*if (PageSize.HasValue) // No need to implement paging as list should contain current view
-            {
-                query = query.Skip(PageSize.Value * (PageIndex - 1)).Take(PageSize.Value).ToList();
-            }*/
-
-            Items = CastItems(query.AsQueryable()).ToList();
-        }
-
         protected abstract IQueryable<TOut> CastItems(IQueryable<TSource> source);
 
-        public virtual Expression<Func<TSource, object>> SortExpression { get => _sortExpression; set => _sortExpression = value; }
+        public virtual Expression<Func<TSource, object>> GetSortExpression()
+        {
+            return _sortExpression;
+        }
 
         [DataMember]
         public List<TOut> Items { get; set; }
@@ -189,56 +134,30 @@ namespace RCommon.Models
             PaginateQueryable(source, paginatedListRequest, skipTotal);
         }
 
-        /// <summary>
-        /// Accepts <see cref="IPaginatedList{T}"/> and overrides total record count with the total count provided in the parameter. 
-        /// </summary>
-        /// <param name="source">Pre-filtered list of data.</param>
-        /// <param name="paginatedListRequest">Request model that contains populated state date for page number, page size, sort by, etc.</param>
-        /// <param name="totalCount">Total count of records contained by pre-filtered data source. This should be different than the actual record 
-        /// count in <see cref="IPaginatedList{T}"/></param>
-        /// <param name="skipSort">Instructions on whether or not to skip sorting. Default is false</param>
-        /// <remarks></remarks>
-        protected PaginatedListModel(IPaginatedList<TSource> source, PaginatedListRequest paginatedListRequest, int totalCount, bool skipSort = false)
-        {
-            PaginateList(source, paginatedListRequest, totalCount, skipSort);
-        }
-
         private IQueryable<TSource> Sort(IQueryable<TSource> source)
         {
 
-            if (this.SortExpression == null)
+            if (this._sortExpression == null)
             {
                 return source;
             }
 
             return SortDirection == SortDirectionEnum.Descending
-                ? source.OrderByDescending(this.SortExpression)
-                : source.OrderBy(this.SortExpression);
-        }
-
-        private IList<TSource> Sort(IPaginatedList<TSource> source)
-        {
-
-            if (this.SortExpression == null)
-            {
-                return source;
-            }
-
-            var sortFunc = this.SortExpression.Compile(); // How much overhead is this?
-
-            var list = SortDirection == SortDirectionEnum.Descending
-                ? source.OrderByDescending(sortFunc).ToList()
-                : source.OrderBy(sortFunc).ToList();
-            return list;
+                ? source.OrderByDescending(this._sortExpression)
+                : source.OrderBy(this._sortExpression);
         }
 
         protected void PaginateQueryable(IQueryable<TSource> source, PaginatedListRequest paginatedListRequest, bool skipTotal = false, bool skipSort = false)
         {
+            if (source == null)
+            {
+                throw new ArgumentException("Source Data cannot be null");
+            }
+
             if (paginatedListRequest == null)
             {
-                return;
+                throw new ArgumentException("Request input cannot be null");
             }
-            Guard.IsNotNull(source, nameof(source));
 
             SortBy = paginatedListRequest.SortBy ?? "id";
             SortDirection = paginatedListRequest.SortDirection;
@@ -262,38 +181,10 @@ namespace RCommon.Models
             Items = query.ToList();
         }
 
-        protected void PaginateList(IPaginatedList<TSource> source, PaginatedListRequest paginatedListRequest, int totalCount,
-            bool skipSort = false)
+        public virtual Expression<Func<TSource, object>> GetSortExpression()
         {
-            if (paginatedListRequest == null)
-            {
-                return;
-            }
-            Guard.IsNotNull(source, nameof(source));
-
-            SortBy = paginatedListRequest.SortBy ?? "id";
-            SortDirection = paginatedListRequest.SortDirection;
-
-            PageSize = paginatedListRequest.PageSize;
-            PageNumber = paginatedListRequest.PageNumber;
-
-            if (totalCount > 0)
-            {
-                TotalCount = totalCount;
-                TotalPages = TotalCount / PageSize + (TotalCount % PageSize > 0 ? 1 : 0) ?? 1;
-            }
-
-            var query = skipSort ? source : Sort(source);
-
-            /*if (PageSize.HasValue) // No need to implement paging as list should contain current view
-            {
-                query = query.Skip(PageSize.Value * (PageIndex - 1)).Take(PageSize.Value).ToList();
-            }*/
-
-            Items = query.AsQueryable().ToList();
+            return _sortExpression;
         }
-
-        public virtual Expression<Func<TSource, object>> SortExpression { get => _sortExpression; set => _sortExpression = value; }
 
         [DataMember]
         public List<TSource> Items { get; set; }
